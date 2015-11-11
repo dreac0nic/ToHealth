c7a216103119c7f905b64ab45962e16f4c65ee00
Reed Lawrence <reedlawrence@letu.edu>
2015-11-10 19:49:30 -0600

    Created content and foreign keys in models
    Included constraints and foreign keys in models

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,4 @@
 class Question < ActiveRecord::Base
+	has_many :answer
+	validates :questionText, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ActiveRecord::Base
+	has_many :todos
+	has_many :answers
+	validates :userName, presence: true
+	validates :firstName, presence: true
+	validates :lastName, presence: true
+	validates :password, presence: true
 end
