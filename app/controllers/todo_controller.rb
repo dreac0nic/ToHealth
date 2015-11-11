4bea16f3163b3ff5d0f6e3f18a690a47a2939111
Sherlock <jarrettergolocke@gmail.com>
2015-11-11 16:40:00 -0600

    Committing the start of something new.

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,2 +1,22 @@
 class TodoController < ApplicationController
+    before_action :all_todos, only: [ :index, :create ]
+    respond_to :html, :js
+
+    def new
+        @todo = Todo.new
+    end
+
+    def create
+        @todo = Todo.create( todo_params )
+    end
+
+    private
+
+    def all_todos
+        @todos = Todo.where( :user_id => params[ :id ] )
+    end
+
+    def todo_params
+        params.require( :todo ).permit( :name )
+    end
 end
