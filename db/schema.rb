2dc13959cfc1d57e6e1d1474f1190197cc64c3d0
Sherlock <jarrettergolocke@gmail.com>
2015-11-12 18:20:09 -0600

    stashed change conflict resolution.

--- db/schema.rb
+++ db/schema.rb
@@ -41,12 +41,12 @@ ActiveRecord::Schema.define(version: 20151111010451) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "userName",       null: false
-    t.string   "firstName",      null: false
-    t.string   "lastName",       null: false
-    t.string   "passwordDigest", null: false
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
+    t.string   "userName",        null: false
+    t.string   "firstName",       null: false
+    t.string   "lastName",        null: false
+    t.string   "password_digest", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
