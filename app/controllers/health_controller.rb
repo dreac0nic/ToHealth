76ef8bb171141edc39b2d00bee15931df8259e8b
Breven <breven95@comcast.net>
2015-11-12 20:25:58 -0600

    Implemented authorization. Added users page to list all users in database

--- app/controllers/health_controller.rb
+++ app/controllers/health_controller.rb
@@ -1,2 +1,19 @@
 class HealthController < ApplicationController
+
+  before_action :require_admin, only:[:edit_questions]
+
+  def index
+    @answers = Answer.all
+    @questions = Question.all
+  end
+
+  def show
+    @question = Question.find(params[:id])
+    @answer = Answer.find(params[:id])
+  end
+
+  def edit_questions
+    @question = Question.find(params[:id])
+  end
+
 end
