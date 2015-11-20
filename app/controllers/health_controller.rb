0cc3b7fc22684753a629a6b31ac9a8bebb53d876
Reed Lawrence <reedlawrence@letu.edu>
2015-11-19 19:22:11 -0600

    Enabled the answers to be queried by the user_id

--- app/controllers/health_controller.rb
+++ app/controllers/health_controller.rb
@@ -2,15 +2,15 @@ class HealthController < ApplicationController
   before_action :require_admin, only:[:edit_questions]
 
   def index
-    #@answers = Answer.where(:user_id => @user_id)
-    @answers = Answer.all
+    @answers = Answer.where(:user_id => session[:user_id_id])
+    #@answers = Answer.all
     @questions = Question.all
   end
 
   def show
     @question = Question.all
-    #@answer = Answer.where(:user_id => @user_id)
-    @answer = Answer.all
+    @answer = Answer.where(:user_id => session[:user_id_id])
+    #@answer = Answer.all
   end
 
   def edit_questions
